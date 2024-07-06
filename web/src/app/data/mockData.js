0d64a20fce2222b27e0f4a27e040fc83b31aa943
TinTin1801 <phucblack1801gmail.com>
2024-07-07 04:21:46 +0700

    mockData.js

---
+++ web/src/app/data/mockData.js
@@ -0,0 +1,136 @@
+import { tokens } from "../theme";
+
+export const generalData = [
+  {
+    id: "3260-96304853",
+    dateOfBirth: "15 Tháng 5 1959",
+    location: "Hà Nội, Việt Nam",
+    firstName: "Nguyễn",
+    lastName: "Thị Mai",
+    sex: "Nữ",
+    diseases: ["Tăng huyết áp", "Tiểu đường"],
+    enteredHospital: "15 Tháng 6 2022",
+    leftHospital: "25 Tháng 6 2022"
+  },
+  {
+    id: "4250-85201347",
+    dateOfBirth: "22 Tháng 1 1980",
+    location: "Hồ Chí Minh, Việt Nam",
+    firstName: "Trần",
+    lastName: "Văn Minh",
+    sex: "Nam",
+    diseases: ["Hen suyễn"],
+    enteredHospital: "25 Tháng 5 2022",
+    leftHospital: "5 Tháng 6 2022"
+  },
+  {
+    id: "5190-76403192",
+    dateOfBirth: "8 Tháng 3 1972",
+    location: "Đà Nẵng, Việt Nam",
+    firstName: "Lê",
+    lastName: "Thị Hồng",
+    sex: "Nữ",
+    diseases: ["Ung thư vú"],
+    enteredHospital: "20 Tháng 4 2022",
+    leftHospital: "30 Tháng 4 2022"
+  },
+  {
+    id: "6730-20184567",
+    dateOfBirth: "30 Tháng 10 1995",
+    location: "Cần Thơ, Việt Nam",
+    firstName: "Phạm",
+    lastName: "Văn Long",
+    sex: "Nam",
+    diseases: ["Cúm"],
+    enteredHospital: "6 Tháng 6 2022",
+    leftHospital: "16 Tháng 6 2022"
+  },
+  {
+    id: "7840-96104782",
+    dateOfBirth: "12 Tháng 7 1987",
+    location: "Hải Phòng, Việt Nam",
+    firstName: "Hoàng",
+    lastName: "Thị Lan",
+    sex: "Nữ",
+    diseases: ["COVID-19"],
+    enteredHospital: "15 Tháng 5 2022",
+    leftHospital: "25 Tháng 5 2022"
+  },
+  {
+    id: "8190-12503764",
+    dateOfBirth: "25 December 1965",
+    location: "Huế, Việt Nam",
+    firstName: "Vũ",
+    lastName: "Văn An",
+    sex: "Nam",
+    diseases: ["Đau tim"],
+    enteredHospital: "6 Tháng 3 2022",
+    leftHospital: "16 Tháng 3 2022"
+  },
+  {
+    id: "9320-74203681",
+    dateOfBirth: "3 Tháng 8 1978",
+    location: "Nha Trang, Việt Nam",
+    firstName: "Đỗ",
+    lastName: "Thị Hạnh",
+    sex: "Nữ",
+    diseases: ["Virus Zika"],
+    enteredHospital: "15 Tháng 2 2022",
+    leftHospital: "25 Tháng 2 2022"
+  },
+  {
+    id: "1040-85201974",
+    dateOfBirth: "17 Tháng 4 1992",
+    location: "Buôn Ma Thuột, Việt Nam",
+    firstName: "Bùi",
+    lastName: "Văn Phúc",
+    sex: "Nam",
+    diseases: ["Sốt rét"],
+    enteredHospital: "10 Tháng 7 2022",
+    leftHospital: "20 Tháng 7 2022"
+  },
+  {
+    id: "1150-96302481",
+    dateOfBirth: "11 Tháng 9 1983",
+    location: "Đà Lạt, Việt Nam",
+    firstName: "Ngô",
+    lastName: "Thị Thảo",
+    sex: "Nữ",
+    diseases: ["Viêm phổi"],
+    enteredHospital: "20 Tháng 8 2022",
+    leftHospital: "30 Tháng 8 2022"
+  },
+  {
+    id: "1260-74503821",
+    dateOfBirth: "6 Tháng 6 1975",
+    location: "Vũng Tàu, Việt Nam",
+    firstName: "Dương",
+    lastName: "Văn Khôi",
+    sex: "Nam",
+    diseases: ["Lao"],
+    enteredHospital: "10 Tháng 9 2022",
+    leftHospital: "20 Tháng 9 2022"
+  }
+];
+
+export const patientTreatmentData = [
+
+  {
+    id: "Tiến trình",
+    color: tokens("dark").greenAccent[500],
+    data: [
+      { x: "Jan", y: 0 },
+      { x: "Feb", y: 5 },
+      { x: "March", y: 10 },
+      { x: "Aprl", y: 25 },
+      { x: "May", y: 40 },
+      { x: "June", y: 55 },
+      { x: "Jully", y: 80 },
+      { x: "August", y: 85 },
+      { x: "Sep", y: 90 },
+      { x: "Oct", y: 95 },
+      { x: "Nov", y: 60 },
+      { x: "Dec", y: 68 },
+    ],
+  }
+];
